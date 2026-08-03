dfafabe84dd08f92a264d9d6dd602dddb5b7658c
Your Name <you@example.com>
2026-08-03 16:04:35 -0500

    Adds base canonical custom character.

--- src/profiles/artwork.ts
+++ src/profiles/artwork.ts
@@ -3,7 +3,7 @@ import type { BuiltInArtwork } from "./types";
 export const BUILT_IN_ARTWORK = Object.freeze({
     "blooky-shimeji": Object.freeze({
         id: "blooky-shimeji",
-        src: "/media/Blooky Shimeji.png",
+        src: "/media/companions/blooky/spritesheet.png",
         width: 1024,
         height: 1152,
     }),
